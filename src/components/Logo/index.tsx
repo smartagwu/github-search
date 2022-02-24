70cf3472cbb6f22053e80182d27957dcde682310
smart.agwu <abiribA123>
2022-02-24 19:19:27 +0100

    feta(logo): Update logo component to for test

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import icon from "../../assets/images/GitHub_Mark.png";
-import logo from "../../assets/images/GitHub_Logo.png";
-import { memoComponent } from "../memo-component";
+import React, { CSSProperties } from "react";
+import icon from "./images/GitHub_Mark.png";
+import logo from "./images/GitHub_Logo.png";
+import { memoComponent } from "../../utils/memoComponent";
 
 interface Props {
   height?: string;
+  style?: CSSProperties;
 }
 
 function Logo(props: Props) {
-  const { height } = props;
+  const { height, style } = props;
 
   return (
-    <div data-testid="logo" style={{ display: "flex", height: `${height || "70px"}` }}>
+    <div data-testid="logo" style={{ ...style, display: "flex", height: `${height || "50px"}` }}>
       <img src={icon} alt="github icon" />
       <img src={logo} alt="github logo" />
     </div>
