13916a47aa9e9888d389a3fee40be6fcee547dc4
smart.agwu <abiribA123>
2022-02-24 19:28:34 +0100

    feta(store): Update app redux store

--- src/store/RootReducer.ts
+++ src/store/RootReducer.ts
@@ -1,5 +1,14 @@
 import { combineReducers } from "redux";
+import githubResultReducer from "../screens/GithubResults/presentation/store/reducer";
+import loginReducer from "../screens/Login/presentation/store/reducer";
+import appReducer from "../screens/RootView/presentation/store/reducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  app: appReducer,
+  login: loginReducer,
+  search: githubResultReducer
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
--- src/store/RootSaga.ts
+++ src/store/RootSaga.ts
@@ -1,7 +1,20 @@
 import { all } from "redux-saga/effects";
+import {
+  triggerSearchUserSaga,
+  triggerSearchRepositorySaga
+} from "../screens/GithubResults/presentation/store/saga";
+import {
+  triggerGetAccessTokenSaga,
+  triggerGetUserProfileSaga
+} from "../screens/Login/presentation/store/saga";
 
 function* rootSaga(dispatch: any) {
-  yield all([]);
+  yield all([
+    triggerGetAccessTokenSaga(),
+    triggerGetUserProfileSaga(),
+    triggerSearchUserSaga(),
+    triggerSearchRepositorySaga()
+  ]);
 }
 
 export default rootSaga;
